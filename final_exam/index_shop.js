320080c8868c826a851e27355659a07aa96e8c72
eunji yang <eunji9128@gmail.com>
2022-12-30 22:32:03 +0900

    final exam shop - cart drag function

--- final_exam/index_shop.js
+++ final_exam/index_shop.js
@@ -15,7 +15,7 @@ function product_dp(obj, num) {
 
     for (var i = 0; i < num; i++) {
         product_layout = `
-            <div class="dp-card">
+            <div class="dp-card" draggable="true" data-id="${obj.products[i].id}">
             <img src="img/${obj.products[i].photo}">
             <h3 class="fs-5 mt-2 fw-bold">${obj.products[i].title}</h3>
             <p class="mt-2">${obj.products[i].brand}</p>
@@ -56,7 +56,7 @@ function product_sch(arr, num, start, end) {
 
     for (var i = 0; i < num; i++) {
         product_layout = `
-            <div class="dp-card">
+            <div class="dp-card" draggable="true" data-id="${obj.products[i].id}">
             <img src="img/${arr[i].photo}">
             <h3 class="fs-5 mt-2 fw-bold">${arr[i].title.slice(0, start[i])}<span style="background-color: yellow;">${arr[i].title.slice(start[i],end[i]+1)}</span>${arr[i].title.slice(end[i]+1,arr[i].title.length)}</h3>
             <p class="mt-2">${arr[i].brand}</p>
@@ -67,3 +67,53 @@ function product_sch(arr, num, start, end) {
         document.querySelector('.dp-box').insertAdjacentHTML('beforeend', product_layout);
     }
 };
+
+// draggable items
+
+var cart_item = [];
+
+document.querySelector('.dp-box').ondragstart = function(e) {
+    e.dataTransfer.setData('id', e.target.dataset.id);
+};
+
+document.querySelector('.dropzone').ondragover = function(e) {
+    e.preventDefault();
+};
+
+document.querySelector('.dropzone').ondrop = function(e) {
+    this.innerHTML = '';
+
+    var id = e.dataTransfer.getData('id');
+    if ( cart_item.length == 0 ) {
+        cart_item.push({'id': products.products[id].id, 'title': products.products[id].title, 'amount': 1});
+        console.log(cart_item);
+    } else {
+        cart_check(cart_item, id);
+    }
+
+    for ( var i = 0; i < cart_item.length; i++ ) {
+        var id = cart_item[i].id;
+        console.log(id);
+        product_layout = `
+                <div class="dp-card" draggable="true" style="color: black;">
+                    <img src="img/${products.products[id].photo}">
+                    <h3 class="fs-5 mt-2 fw-bold">${products.products[id].title}</h3>
+                    <p class="mt-2">${products.products[id].brand}</p>
+                    <h4 class="fs-6 mt-3 fw-semibold">${products.products[id].price}</h4>
+                    <p class="white-box mt-3 p-1">${cart_item[i].amount}</p>
+                </div>`; 
+        console.log(products.products[id].title);
+        this.insertAdjacentHTML('beforeend', product_layout);
+    }
+
+};
+
+function cart_check(arr, id) {
+    for ( var i = 0; i < arr.length; i++ ) {
+        if ( cart_item[i].id == products.products[id].id ) {
+            cart_item[i].amount += 1;
+            return i
+        }
+    }
+    cart_item.push({'id': products.products[id].id, 'title': products.products[id].title, 'amount': 1});
+}
